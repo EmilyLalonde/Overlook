7cb56c8c325f7f11f94f784bb83efce66169994a
Emily <lalonde0902@gmail.com>
2019-07-28 18:29:22 -0600

    Add date input to display a calender when the user is allowed to chose dates to view options. There is a jQuery function that captures the date

--- src/bookings.js
+++ src/bookings.js
@@ -7,7 +7,7 @@ class Bookings {
     let today = new Date();
     let year = today.getFullYear();
     let month = String(today.getMonth() + 1).padStart(2, '0');
-    let day = String(today.getDate())
+    let day = today.getDate()
     return `${year}/${month}/${day}`
   }
   findMostPopularBookingDate() {
